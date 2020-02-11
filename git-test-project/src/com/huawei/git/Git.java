23a0f6055d9590203123a3771733c7a31ba45077
wxm <wxm111@qq.com>
2020-02-11 21:46:30 +0800

    添加第一次

--- git-test-project/src/com/huawei/git/Git.java
+++ git-test-project/src/com/huawei/git/Git.java
@@ -4,4 +4,9 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello,git!");
     }
+
+    public void test(){
+        int i = 0;
+        System.out.println(i);
+    }
 }
